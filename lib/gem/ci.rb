b0118f3909ddbf630032569777b40e6a83bedf5e
Huy Nguyen <patrick204nqh@gmail.com>
2025-07-23 21:17:30 +0700

    Initialize gem structure with essential files and configurations

---
+++ lib/gem/ci.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "ci/version"
+
+module Gem
+  module Ci
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/gem/ci_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Gem::Ci do
+  it "has a version number" do
+    expect(Gem::Ci::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
